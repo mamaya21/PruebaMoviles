153928e3d60b697c847d9d21216162ed59cdbef2
Marco Amaya Quiroz <marco1021tam@gmail.com>
2017-03-02 14:53:54 -0500

    cambios

---
+++ pg2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<title>Demo</title>
+<link href="jquery-mobile/jquery.mobile-1.0.min.css" rel="stylesheet" type="text/css"/>
+
+<script src="jquery-mobile/jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery-mobile/jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body> 
+
+<?php
+	?>
+    	<div data-role="page" id="page2">
+	<div data-role="header">
+		<h1>Página dos</h1>
+        <h2> <a href="tel:+51989583945;postd=4">Llámando al RPC de VS</a></h2>  
+	</div>
+	<div data-role="content">	
+		Contenido		
+	</div>
+	<div data-role="footer">
+		<h4>Pie de página</h4>
+	</div>
+</div>
+
+    <?php
+?>
+
+</body>
+</html>
